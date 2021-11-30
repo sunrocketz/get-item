3a12ad1f4447174191b963cfd290d2a7155ee880
Truong Nguyen <truong.mnguyen92@gmail.com>
2021-12-01 06:22:39 +0700

    fix bundler options

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,28 @@
 const { RollupOptions } = require('rollup')
+const dts = require('rollup-plugin-dts')
 const typescript = require('@rollup/plugin-typescript')
 const { terser } = require('rollup-plugin-terser')
 
-/** @type {RollupOptions} */
-const configs = {
-  input: 'index.ts',
-  output: [
-    {
-      format: 'commonjs',
-      file: 'dist/index.cjs',
-      exports: 'default',
-    },
-    {
-      format: 'esm',
-      file: 'dist/index.js',
-    },
-  ],
-  plugins: [typescript(), terser({ keep_fnames: true, keep_classnames: true })],
-}
+/** @type {RollupOptions[]} */
+const configs = [
+  {
+    input: 'index.ts',
+    output: [
+      {
+        format: 'commonjs',
+        file: 'dist/index.cjs',
+        exports: 'default',
+      },
+      {
+        format: 'esm',
+        file: 'dist/index.js',
+      },
+    ],
+    plugins: [
+      typescript({ tsconfig: './tsconfig.json' }),
+      terser({ keep_fnames: true, keep_classnames: true }),
+    ],
+  },
+]
 
 module.exports = configs
